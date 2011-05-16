7dc7dfbd7394a6734f594b23bdb0e21db81f0408
Jlake Ou <ouzhiwei@gmail.com>
2011-05-16 16:58:50 +0900

    replace HelloWorldLayer with GameLayer

---
+++ GameLayer.h
@@ -0,0 +1,18 @@
+//
+//  GameLayer.h
+//  CocosDD
+//
+//  Created by 欧 on 11/05/16.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "cocos2d.h"
+
+@interface GameLayer : CCLayer {
+@private
+    CGSize winSize;
+}
+
++(CCScene *) scene;
+
+@end
