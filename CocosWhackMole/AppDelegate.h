d0c287bd0cf3e8d136934987505c9b9f68bb1633
Jlake Ou <ouzhiwei@gmail.com>
2011-05-16 16:49:12 +0900

    Initial Commit

---
+++ CocosWhackMole/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  CocosWhackMole
+//
+//  Created by 欧 on 11/05/16.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ CocosWhackMole/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  CocosWhackMole
+//
+//  Created by 欧 on 11/05/16.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
